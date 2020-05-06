9bf80b75bcdbb11ad602fb903172c60da6ab0be2
Luz <luz.catzoli@gmail.com>
2020-05-06 09:41:11 -0500

    Base de Datos

---
+++ SQL/DB_rest.sql
@@ -0,0 +1,57 @@
+drop database restaurante;
+create database restaurante;
+use restaurante;
+
+CREATE TABLE Usuario
+(
+ id_usuario Int NOT NULL AUTO_INCREMENT,
+ name    Varchar(45) NOT NULL ,
+ mail     Varchar(45) NOT NULL ,
+ password Varchar(45) NOT NULL ,
+ type Varchar(45) NOT NULL ,
+
+PRIMARY KEY (id_usuario)
+);
+
+
+CREATE TABLE Reservacion
+(
+  id_reservacion Int NOT NULL AUTO_INCREMENT,
+ id_usuario   Int NOT NULL ,
+ id_mesa   Int NOT NULL ,
+ fecha   date  NOT NULL,
+ horario time NOT NULL ,
+ npersonas     Int NOT NULL ,
+
+PRIMARY KEY (id_reservacion)
+
+);
+
+CREATE TABLE Feedback
+(
+  id_feedback Int NOT NULL AUTO_INCREMENT,
+ id_usuario   Int NOT NULL ,
+ comentario   date  NOT NULL,
+ star time NOT NULL ,
+ fecha date   NOT NULL ,
+
+PRIMARY KEY (id_feedback)
+);
+CREATE TABLE Mesa
+(
+  id_mesa Int NOT NULL AUTO_INCREMENT,
+ capacidad Int   NOT NULL ,
+
+PRIMARY KEY (id_mesa)
+);
+
+
+
+
+
+
+Alter table Reservacion add Foreign Key (id_usuario) references Usuario (id_usuario);
+Alter table Reservacion add Foreign Key (id_mesa) references Mesa (id_mesa);
+Alter table Feedback add Foreign Key (id_usuario) references Usuario (id_usuario);
+
+
